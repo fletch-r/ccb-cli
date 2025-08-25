3a18ae8e206947fb25f723bb8949eca19dabbe03
fletchers <ath3ris@proton.me>
2025-08-25 21:25:53 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+/*
+Creates to use
+    - Clap
+        - Command Line Parser - helps with reducing if statements
+        - To be used in the future probably, right now the cli won't have any args
+    - git2
+        - Shortcut to read `git status`
+        - Should use now
+    - console
+        - Helps to style console output
+        - Maybe will use
+*/
+
+// Add command to PATH
+
+// Ask what files you would like to add: "git add <file_path>"
+
+// Prompt for a commit message
+
+// Use regex to get branch ticket/issue id
