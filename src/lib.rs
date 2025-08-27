440d6f29fcfd2fd502d0b57e9d33a0d457915461
fletchers <ath3ris@proton.me>
2025-08-27 16:14:36 +0100

    commit message

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use dialoguer::MultiSelect;
+use git2::{Repository, Status};
+
 /*
 Creates to use
     - Clap
@@ -10,6 +13,134 @@ Creates to use
         - Helps to style console output
         - Maybe will use
 */
+
+fn get_statuses() -> (Vec<String>, Vec<bool>) {
+    let repo = Repository::open(".").unwrap();
+    let statuses = repo.statuses(None).unwrap();
+
+    if statuses.is_empty() {
+        println!("{}", console::style("✔ working tree clean").green());
+    }
+
+    let mut items = vec![];
+    let mut default_checked = vec![];
+
+    for entry in statuses.iter() {
+        let path = entry
+            .head_to_index()
+            .and_then(|d| d.new_file().path())
+            .or_else(|| entry.index_to_workdir().and_then(|d| d.new_file().path()))
+            .or_else(|| entry.index_to_workdir().and_then(|d| d.old_file().path()))
+            .map(|p| p.display().to_string())
+            .unwrap_or_else(|| "<unknown>".into());
+
+        let s = entry.status();
+
+        let b = is_staged(s);
+
+        items.push(path);
+        default_checked.push(b);
+    }
+
+    (items, default_checked)
+}
+
+pub fn choose_files() -> Vec<String> {
+    let (items, defaults) = get_statuses();
+
+    let selection = MultiSelect::new()
+        .with_prompt("Choose files to stage")
+        .items(&items)
+        .defaults(&defaults)
+        .interact()
+        .unwrap();
+
+    let mut paths: Vec<String> = vec![];
+
+    for i in selection {
+        println!("{}", items[i]);
+        paths.push(items[i].clone());
+    }
+
+    paths
+}
+
+fn is_staged(s: Status) -> bool {
+    // Index (staged)
+    // If it is staged, it should be pre-selected when running ccb
+
+    let index = vec![
+        Status::INDEX_NEW,
+        Status::INDEX_MODIFIED,
+        Status::INDEX_DELETED,
+        Status::INDEX_RENAMED,
+        Status::INDEX_TYPECHANGE,
+        Status::CONFLICTED,
+        Status::IGNORED,
+    ];
+
+    for status in index {
+        if s.contains(status) {
+            return true;
+        }
+    }
+
+    if s.contains(Status::WT_NEW) && s.contains(Status::INDEX_NEW) {
+        true
+    } else if s.contains(Status::WT_MODIFIED) && s.contains(Status::INDEX_MODIFIED) {
+        return true;
+    } else if s.contains(Status::WT_DELETED) && s.contains(Status::INDEX_DELETED) {
+        return true;
+    } else if s.contains(Status::WT_RENAMED) && s.contains(Status::INDEX_RENAMED) {
+        return true;
+    } else if s.contains(Status::WT_TYPECHANGE) && s.contains(Status::INDEX_TYPECHANGE) {
+        return true;
+    } else if s.contains(Status::WT_NEW) && !s.intersects(Status::INDEX_NEW) {
+        return true;
+    } else {
+        let worktree = vec![
+            Status::WT_NEW,
+            Status::WT_MODIFIED,
+            Status::WT_DELETED,
+            Status::WT_RENAMED,
+            Status::WT_TYPECHANGE,
+            Status::CONFLICTED
+        ];
+
+        for status in worktree {
+            if s.contains(status) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use git2::Status;
+    use crate::get_statuses;
+    use crate::is_staged;
+
+    #[test]
+    fn statuses_has_length() {
+        let (items, _) = get_statuses();
+        assert!(items.len() > 0);
+    }
+
+    #[test]
+    fn status_returns_true() {
+        let value = is_staged(Status::INDEX_MODIFIED);
+        assert!(value);
+    }
+
+    #[test]
+    fn status_returns_false() {
+        let value = is_staged(Status::WT_MODIFIED);
+        assert_eq!(value, false);
+    }
+}
 
 // Add command to PATH
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,121 @@
-fn main() {
-    println!("Hello, world!");
+mod example;
+
+use std::env;
+use git2::{IndexAddOption, Repository, Signature, Branch};
+use ccb_cli::choose_files;
+use log::debug;
+
+fn git_commit_push(repo: &Repository, my_branch: &str) {
+    let mut index = repo.index().unwrap();
+    let tree = repo
+        .find_tree(index.write_tree().unwrap())
+        .unwrap();
+    let author = Signature::now("fletchers", "ath3ris@proton.me").unwrap();
+
+    let mut update_ref = Some("HEAD");
+
+    if let Ok(head) = repo.head() {
+        update_ref = Some("HEAD");
+    } else {
+        update_ref = None; // no HEAD = first commit
+    }
+
+    let parent_commit = repo.head().unwrap().peel_to_commit().unwrap();
+
+    let commit_oid = repo
+        .commit(update_ref, &author, &author, "commit message", &tree, &[&parent_commit])
+        .unwrap();
+
+    let mut origin = repo.find_remote("origin").unwrap();
+    origin.push(&["refs/heads/".to_owned() + my_branch], None).unwrap();
 }
+
+fn git_add_all(repo: &Repository, paths: &Vec<String>) {
+    let mut index = repo.index().unwrap();
+    index
+        .add_all(&["."], git2::IndexAddOption::DEFAULT, None)
+        .unwrap();
+    index.write().unwrap();
+}
+
+fn main() {
+    let paths = choose_files();
+
+    println!("Chosen paths to commit - {:#?}", paths);
+
+    let mut callbacks = git2::RemoteCallbacks::new();
+    callbacks.credentials(git_credentials_callback);
+
+    let mut opts = git2::FetchOptions::new();
+    opts.remote_callbacks(callbacks);
+    opts.download_tags(git2::AutotagOption::All);
+
+    let mut builder = git2::build::RepoBuilder::new();
+    builder.fetch_options(opts);
+    builder.branch("master");
+
+    let repo = Repository::open(".").unwrap();
+
+    git_add_all(&repo, &paths);
+
+    git_commit_push(&repo, "master");
+
+    // let current_branch = index.write_tree().unwrap();
+
+    // git_commit_push(&repo, current_branch.to_string().as_str());
+}
+
+pub fn git_credentials_callback(
+    _user: &str,
+    _user_from_url: Option<&str>,
+    _cred: git2::CredentialType,
+) -> Result<git2::Cred, git2::Error> {
+    let user = _user_from_url.unwrap_or("git");
+
+    if _cred.contains(git2::CredentialType::USERNAME) {
+        return git2::Cred::username(user);
+    }
+
+    match env::var("GPM_SSH_KEY") {
+        Ok(k) => {
+            debug!("authenticate with user {} and private key located in {}", user, k);
+            git2::Cred::ssh_key(user, None, std::path::Path::new(&k), None)
+        },
+        _ => Err(git2::Error::from_str("unable to get private key from GPM_SSH_KEY")),
+    }
+}
+
+// fn get_or_init_repo(cache: &std::path::Path, remote: &String) -> Result<git2::Repository, git2::Error> {
+//     let data_url = match Url::parse(remote) {
+//         Ok(data_url) => data_url,
+//         Err(e) => panic!("failed to parse url: {}", e),
+//     };
+//     let path = cache.deref().join(data_url.host_str().unwrap()).join(&data_url.path()[1..]);
+//
+//     if path.exists() {
+//         debug!("use existing repository already in cache {}", path.to_str().unwrap());
+//         return git2::Repository::open(path);
+//     }
+//
+//     let mut callbacks = git2::RemoteCallbacks::new();
+//     callbacks.credentials(git_credentials_callback);
+//
+//     let mut opts = git2::FetchOptions::new();
+//     opts.remote_callbacks(callbacks);
+//     opts.download_tags(git2::AutotagOption::All);
+//
+//     let mut builder = git2::build::RepoBuilder::new();
+//     builder.fetch_options(opts);
+//     builder.branch("master");
+//
+//     debug!("start cloning repository {} in {}", remote, path.to_str().unwrap());
+//
+//     match builder.clone(remote, &path) {
+//         Ok(r) => {
+//             debug!("repository cloned");
+//
+//             Ok(r)
+//         },
+//         Err(e) => Err(e)
+//     }
+// }
